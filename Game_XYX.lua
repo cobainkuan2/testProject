1358d2fe17b80d60c123c692e51ae591eb9e7312
cobainkuan2 <36189143+cobainkuan2@users.noreply.github.com>
2018-02-06 17:27:38 +0800

    Add files via upload

---
+++ Game_XYX.lua
@@ -0,0 +1,9 @@
+local Game_XYX = {}
+Game_XYX.XYX_GameStepID = 10001
+Game_XYX.XYX_GameSettingID = 10002
+Game_XYX.XYX_GameRetID = 10003
+Game_XYX.XYX_NotifyUserBaseScoreID = 10004
+Game_XYX.XYX_NotifyGameBaseScoreID = 10005
+Game_XYX.XYX_UserBaseScoreID = 10006
+Game_XYX.XYX_GameSceneID = 10007
+return Game_XYX
