64e47a4b198144f0b1ba147256ae089bfd236b61
cobainkuan2 <36189143+cobainkuan2@users.noreply.github.com>
2018-02-06 17:41:56 +0800

    Update Game_XYX.lua

--- Game_XYX.lua
+++ Game_XYX.lua
@@ -1,5 +1,5 @@
 local Game_XYX = {}
-Game_XYX.XYX_GameStepID = 10001
+Game_XYX.XYX_GameStepID = 100012
 Game_XYX.XYX_GameSettingID = 10002
 Game_XYX.XYX_GameRetID = 10003
 Game_XYX.XYX_NotifyUserBaseScoreID = 10004
